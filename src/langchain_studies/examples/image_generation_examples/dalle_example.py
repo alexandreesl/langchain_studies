02bce52ec879b6b483011c69dcfbcac4131eae7f
Alexandre <alexandreesl@gmail.com>
2026-04-07 19:55:50 -0300

    adding image examples

--- src/langchain_studies/examples/image_generation_examples/dalle_example.py
+++ src/langchain_studies/examples/image_generation_examples/dalle_example.py
@@ -2,7 +2,7 @@ from langchain_community.utilities.dalle_image_generator import DallEAPIWrapper
 dalle = DallEAPIWrapper(model="dall-e-3",size="1024x1024",       # Image dimensions
     quality="hd",  )
 # Generate an image
-image_url = dalle.run("A robot cyberpunk girl with a green hair on city night, on top of a motorcycle. Should be a high quality image, cyberpunk 2077 style.")
+image_url = dalle.run("A robot cyberpunk girl with a green hair on city night, on top of a motorcycle. Should be a high quality image.")
 # Display the image in a notebook
 from IPython.display import Image, display
 display(Image(url=image_url))
